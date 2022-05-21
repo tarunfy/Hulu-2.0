365d63501713d2ba362b94524ffa3987d77eeeb5
tarunfy <tarunsharma8920@gmail.com>
2022-05-21 18:10:00 +0530

    final

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import { forwardRef } from "react";
 
-const Thumbnail = ({ result }) => {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+      className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+    >
       <Image
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
@@ -28,6 +32,6 @@ const Thumbnail = ({ result }) => {
       </div>
     </div>
   );
-};
+});
 
 export default Thumbnail;
